1cdda90dc92ad9b8aaa79c7cfdde07ba05081096
princecollins <kidflash78@gmail.com>
2022-07-05 14:40:58 -0400

    ok

--- src/main/java/com/codedifferently/Problem01.java
+++ src/main/java/com/codedifferently/Problem01.java
@@ -2,6 +2,16 @@ package com.codedifferently;
 
 public class Problem01 {
     public static Boolean isPalindrome(int x) {
-        return null;
+        /** first change numbers into string
+         * use string builder to reverse
+         * check old str with new reversed str
+         * if the same then return true*/
+        String pal = String.valueOf(x);
+        StringBuilder str = new StringBuilder(pal);
+        StringBuilder newStr = str.reverse();
+//        if(pal.equals(newStr.toString())) {
+//            return true;
+//        }
+            return pal.equals(newStr.toString()) ? true : false;
     }
 }
